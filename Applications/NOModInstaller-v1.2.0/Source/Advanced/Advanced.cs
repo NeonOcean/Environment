cddb003094de2c7dbd90a3bd44c55139f285141f
NeonOcean <40919586+NeonOcean@users.noreply.github.com>
2020-07-17 12:54:36 -0500

    7/17/2020 12:55 PM Update

---
+++ Applications/NOModInstaller-v1.2.0/Source/Advanced/Advanced.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NOModInstaller {
+	public partial class Advanced : Form {
+		public Advanced () {
+			InitializeComponent();
+		}
+
+		private void Advanced_Load (object sender, EventArgs e) {
+			if(Paths.Sims4Path == null) {
+				UserDirectoryTextbox.Text = "";
+			} else {
+				UserDirectoryTextbox.Text = Paths.Sims4Path.GetPath();
+			}
+		}
+
+		private void UserDirectoryTextboxButton_Click (object sender, EventArgs e) {
+			FolderBrowser.SelectedPath = UserDirectoryTextbox.Text;
+			FolderBrowser.ShowDialog();
+			UserDirectoryTextbox.Text = FolderBrowser.SelectedPath;
+		}
+
+		private void UserDirectoryTextbox_TextChanged (object sender, EventArgs e) {
+			if(UserDirectoryTextbox.Text != "") {
+				ModsDirectoryTextbox.Text = Path.Combine(UserDirectoryTextbox.Text, "Mods");
+			} else {
+				ModsDirectoryTextbox.Text = "/Mods";
+			}
+		}
+
+		private void InstallButton_Click (object sender, EventArgs e) {
+			try {
+				new DirectoryInfo(UserDirectoryTextbox.Text);
+			} catch {
+				MessageBox.Show(string.Format(Localization.GetString("InvalidDirectory"), UserDirectoryTextbox.Text));
+				return;
+			}
+
+			IO.PathContainer sims4Path = new IO.PathContainer(UserDirectoryTextbox.Text);
+			string[] sims4PathSegments = sims4Path.GetSegments();
+
+			if(sims4PathSegments.Length >= 2) {
+				if(sims4PathSegments[sims4PathSegments.Length - 1].ToLower() == "mods") {
+					if(MessageBox.Show(Localization.GetString("SelectedModsFolderMessageText"), "", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) != DialogResult.Yes) {
+						return;
+					}
+				}
+			}
+
+			Paths.Sims4Path = new IO.PathContainer(UserDirectoryTextbox.Text);
+
+
+			Hide();
+			Main.Run();
+			Close();
+		}
+
+		private void AbortButton_Click (object sender, EventArgs e) {
+			Close();
+		}
+
+	}
+}
---
+++ Applications/NOModInstaller-v1.2.0/Source/INI.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NOModInstaller {
+	public static class INI {
+		public static string Read (string section, string key, string filePath) {
+			return Read(section, key, "", filePath);
+		}
+
+		public static string Read (string section, string key, string defaultValue, string filePath) {
+			StringBuilder returnValue = new StringBuilder(255);
+			GetPrivateProfileString(section, key, defaultValue, returnValue, returnValue.Capacity, filePath);
+			return returnValue.ToString();
+		}
+
+		public static void Write (string section, string key, string value, string filePath) {
+			WritePrivateProfileString(section, key, value, filePath);
+		}
+
+		[DllImport("kernel32")]
+		private static extern long WritePrivateProfileString (string section, string key, string value, string filePath);
+		[DllImport("kernel32")]
+		private static extern int GetPrivateProfileString (string section, string key, string defaultValue, StringBuilder returnValue, int returnSize, string filePath);
+	}
+}
